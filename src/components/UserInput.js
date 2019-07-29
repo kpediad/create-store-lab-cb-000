ae236c0374fdd69db1a1cbfc000616b044cf1c44
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-07-29 10:28:24 +0000

    Automatically backed up by Learn

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  state = {
+    username: '',
+    hometown: ''
+  }
+
+  handleOnChangeUsername(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handleOnChangeHometown(event) {
+    this.setState({
+      hometown: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    //this.props.addReview({text: this.state.text, restaurantId: this.props.restaurant.id});
+    this.setState({
+      username: '',
+      hometown: ''
+    });
+  }
+
+  
   render() {
     return(
       <div>
